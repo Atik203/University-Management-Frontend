4d01bb1d96522faeda155b7352d6aa18ebd7febe
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-07-04 20:37:13 +0600

    Render Sidebar Based On Role

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import { routeGenerator } from "../utils/routeGenerator";
 import { adminPaths } from "./admin.routes";
+import { facultyPaths } from "./faculty.routes";
+import { studentPaths } from "./student.routes";
 
 const router = createBrowserRouter([
   {
@@ -12,6 +14,16 @@ const router = createBrowserRouter([
     path: "/admin",
     element: <App />,
     children: routeGenerator(adminPaths),
+  },
+  {
+    path: "/faculty",
+    element: <App />,
+    children: routeGenerator(facultyPaths),
+  },
+  {
+    path: "/student",
+    element: <App />,
+    children: routeGenerator(studentPaths),
   },
 ]);
 
---
+++ src/types/index.ts
@@ -0,0 +1,2 @@
+export * from "./route.types";
+export * from "./sidebar.types";

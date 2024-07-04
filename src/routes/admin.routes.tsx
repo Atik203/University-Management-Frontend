cd285dfce3dfaa1995238916e52a3ecd666c5288
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-07-04 19:03:46 +0600

    added dynamicly mainlayout sidbar items

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -1,9 +1,20 @@
 import { ReactNode } from "react";
+import { NavLink } from "react-router-dom";
 import AdminDashboard from "../pages/admin/AdminDashboard";
 import CreateAdmin from "../pages/admin/CreateAdmin";
 import CreateCourse from "../pages/admin/CreateCourse";
 import CreateFaculty from "../pages/admin/CreateFaculty";
 import CreateStudent from "../pages/admin/CreateStudent";
+
+interface TSidebarItem {
+  key: string;
+  label: ReactNode;
+  children?: TSidebarItem[];
+}
+interface TRouteItem {
+  path: string;
+  element: ReactNode;
+}
 
 const adminPaths = [
   {
@@ -43,13 +54,8 @@ const adminPaths = [
   },
 ];
 
-interface RouteItem {
-  path: string;
-  element: ReactNode;
-}
-
-export const adminRoutes: RouteItem[] = adminPaths.reduce(
-  (acc: RouteItem[], item) => {
+export const adminRoutes: TRouteItem[] = adminPaths.reduce(
+  (acc: TRouteItem[], item) => {
     if (item.path && item.element) {
       acc.push({
         path: item.path,
@@ -63,6 +69,29 @@ export const adminRoutes: RouteItem[] = adminPaths.reduce(
           path: child.path,
           element: child.element,
         });
+      });
+    }
+    return acc;
+  },
+  []
+);
+
+export const adminSidebarItems: TSidebarItem[] = adminPaths.reduce(
+  (acc: TSidebarItem[], item) => {
+    if (item.path && item.name) {
+      acc.push({
+        key: item.name,
+        label: <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>,
+      });
+    }
+    if (item.children) {
+      acc.push({
+        key: item.name,
+        label: item.name,
+        children: item.children.map((child) => ({
+          key: child.name,
+          label: <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>,
+        })),
       });
     }
     return acc;

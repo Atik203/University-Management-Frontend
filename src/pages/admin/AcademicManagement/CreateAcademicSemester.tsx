be80f8e31e109ce0220f78aae1259d3e97247468
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-08-06 18:14:36 +0600

    updated select component

--- src/pages/admin/AcademicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/AcademicManagement/CreateAcademicSemester.tsx
@@ -3,16 +3,25 @@ import { FieldValues, SubmitErrorHandler } from "react-hook-form";
 import OpenSelect from "../../../components/form/OpenSelect";
 import OpenForm from "./../../../components/form/OpenForm";
 
+const nameOptions = [
+  { value: "01", label: "Autumn" },
+  { value: "02", label: "Summer" },
+  { value: "03", label: "Fall" },
+];
+
 const CreateAcademicSemester = () => {
   const onSubmit: SubmitErrorHandler<FieldValues> = (data) => {
-    console.log(data);
+    const semesterData = {
+      name: "",
+      code: "",
+    };
   };
 
   return (
     <Flex justify="center" align="center">
       <Col span={7}>
         <OpenForm onSubmit={onSubmit}>
-          <OpenSelect label="Name" />
+          <OpenSelect label="Name" name="name" options={nameOptions} />
 
           <Button type="primary" htmlType="submit">
             Submit

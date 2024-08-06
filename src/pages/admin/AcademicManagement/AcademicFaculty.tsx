0aa5b87d841efb90cb2a6315ed90a79b6290f389
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-08-07 01:02:54 +0600

    added academic facultyes page

--- src/pages/admin/AcademicManagement/AcademicFaculty.tsx
+++ src/pages/admin/AcademicManagement/AcademicFaculty.tsx
@@ -1,7 +1,31 @@
+import { useGetAllAcademicFacultiesQuery } from "../../../redux/features/admin/academicManagement.api";
+
 const AcademicFaculty = () => {
+  const { data, isFetching, isLoading } =
+    useGetAllAcademicFacultiesQuery(undefined);
+
+  if (isLoading || isFetching) {
+    return <h4>Loading...</h4>;
+  }
+  const academicFaculties = data?.data;
+
   return (
-    <div>
-      <h1>This is AcademicFaculty component</h1>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "1rem",
+        padding: "1rem",
+      }}
+    >
+      <h1>Academic Faculties</h1>
+      {academicFaculties?.map((faculty, index) => (
+        <div key={faculty._id}>
+          <h2>
+            {index + 1}. {faculty.name}
+          </h2>
+        </div>
+      ))}
     </div>
   );
 };

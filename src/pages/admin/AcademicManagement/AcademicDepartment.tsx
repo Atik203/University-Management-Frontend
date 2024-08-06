218559b187eba4fd15fd3d06d37172530da515ff
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-08-07 01:27:45 +0600

    added academicdepartment page

--- src/pages/admin/AcademicManagement/AcademicDepartment.tsx
+++ src/pages/admin/AcademicManagement/AcademicDepartment.tsx
@@ -1,7 +1,33 @@
+import { useGetAllAcademicDepartmentsQuery } from "../../../redux/features/admin/academicManagement.api";
+
 const AcademicDepartment = () => {
+  const { data, isFetching, isLoading } =
+    useGetAllAcademicDepartmentsQuery(undefined);
+
+  const academicDepartments = data?.data;
+
+  if (isLoading || isFetching) {
+    return <h4>Loading...</h4>;
+  }
+
   return (
-    <div>
-      <h1>This is AcademicDepartment component</h1>
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(2, 1fr)",
+        gap: "2rem",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {academicDepartments?.map((department, index) => (
+        <div key={department._id}>
+          <h3>
+            {index + 1} {department.name}
+          </h3>
+          <p>{department.academicFaculty.name}</p>
+        </div>
+      ))}
     </div>
   );
 };

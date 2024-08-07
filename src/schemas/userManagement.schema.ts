82d099220917017814717d9e7ab8088f1ecc3c6f
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-08-07 22:16:36 +0600

    updated schema

--- src/schemas/userManagement.schema.ts
+++ src/schemas/userManagement.schema.ts
@@ -1,7 +1,7 @@
-import { z } from "zod";
+import * as z from "zod";
 
 const userNameSchema = z.object({
-  firstName: z.string({ required_error: "First Name is required" }),
+  firstName: z.string({ required_error: "First Name is required" }).min(1),
   middleName: z.string({ required_error: "Middle name is required" }),
   lastName: z.string({ required_error: "Last name is required" }),
 });
@@ -31,35 +31,33 @@ const localGuardianSchema = z.object({
 });
 
 export const createStudentValidationSchema = z.object({
-  body: z.object({
-    password: z
-      .string()
-      .min(4, "Password must be at least 6 characters")
-      .optional(),
-    student: z.object({
-      name: userNameSchema,
-      gender: z.enum(["male", "female", "other"]),
-      dateOfBirth: z.string().optional(),
-      email: z.string({ required_error: "Email is required" }).email(),
-      contactNo: z.string({ required_error: "Contact number is required" }),
-      emergencyContactNo: z.string({
-        required_error: "Emergency contact number is required",
-      }),
-      bloodGroup: z.enum(["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"]),
-      presentAddress: z.string({
-        required_error: "Present address is required",
-      }),
-      permanentAddress: z.string({
-        required_error: "Permanent address is required",
-      }),
-      guardian: guardianSchema,
-      localGuardian: localGuardianSchema,
-      admissionSemester: z.string({
-        required_error: "Admission semester is required",
-      }),
-      academicDepartment: z.string({
-        required_error: "Academic department is required",
-      }),
+  password: z
+    .string()
+    .min(4, "Password must be at least 6 characters")
+    .optional(),
+  student: z.object({
+    name: userNameSchema,
+    gender: z.enum(["male", "female", "other"]),
+    dateOfBirth: z.string().optional(),
+    email: z.string({ required_error: "Email is required" }).email(),
+    contactNo: z.string({ required_error: "Contact number is required" }),
+    emergencyContactNo: z.string({
+      required_error: "Emergency contact number is required",
+    }),
+    bloodGroup: z.enum(["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"]),
+    presentAddress: z.string({
+      required_error: "Present address is required",
+    }),
+    permanentAddress: z.string({
+      required_error: "Permanent address is required",
+    }),
+    guardian: guardianSchema,
+    localGuardian: localGuardianSchema,
+    admissionSemester: z.string({
+      required_error: "Admission semester is required",
+    }),
+    academicDepartment: z.string({
+      required_error: "Academic department is required",
     }),
   }),
 });
@@ -103,38 +101,33 @@ const updateLocalGuardianSchema = z.object({
 });
 
 export const updateStudentValidationSchema = z.object({
-  body: z.object({
-    student: z.object({
-      name: updateUserNameSchema.optional(),
-      gender: z.enum(["male", "female", "other"]).optional(),
-      dateOfBirth: z.string().optional(),
-      email: z
-        .string({ required_error: "Email is required" })
-        .email()
-        .optional(),
-      contactNo: z
-        .string({ required_error: "Contact number is required" })
-        .optional(),
-      emergencyContactNo: z
-        .string({ required_error: "Emergency contact number is required" })
-        .optional(),
-      bloodGroup: z
-        .enum(["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"])
-        .optional(),
-      presentAddress: z
-        .string({ required_error: "Present address is required" })
-        .optional(),
-      permanentAddress: z
-        .string({ required_error: "Permanent address is required" })
-        .optional(),
-      guardian: updateGuardianSchema.optional(),
-      localGuardian: updateLocalGuardianSchema.optional(),
-      admissionSemester: z
-        .string({ required_error: "Admission semester is required" })
-        .optional(),
-      academicDepartment: z
-        .string({ required_error: "Academic department is required" })
-        .optional(),
-    }),
+  student: z.object({
+    name: updateUserNameSchema.optional(),
+    gender: z.enum(["male", "female", "other"]).optional(),
+    dateOfBirth: z.string().optional(),
+    email: z.string({ required_error: "Email is required" }).email().optional(),
+    contactNo: z
+      .string({ required_error: "Contact number is required" })
+      .optional(),
+    emergencyContactNo: z
+      .string({ required_error: "Emergency contact number is required" })
+      .optional(),
+    bloodGroup: z
+      .enum(["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"])
+      .optional(),
+    presentAddress: z
+      .string({ required_error: "Present address is required" })
+      .optional(),
+    permanentAddress: z
+      .string({ required_error: "Permanent address is required" })
+      .optional(),
+    guardian: updateGuardianSchema.optional(),
+    localGuardian: updateLocalGuardianSchema.optional(),
+    admissionSemester: z
+      .string({ required_error: "Admission semester is required" })
+      .optional(),
+    academicDepartment: z
+      .string({ required_error: "Academic department is required" })
+      .optional(),
   }),
 });

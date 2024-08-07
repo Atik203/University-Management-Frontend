9519ac37cb573511f17467d7ae214324e6e46ba7
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-08-07 21:46:14 +0600

    created student create form

--- src/pages/admin/UserManagement/CreateStudent.tsx
+++ src/pages/admin/UserManagement/CreateStudent.tsx
@@ -1,5 +1,5 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Button, Col, Row } from "antd";
+import { Button, Col, Divider, Row } from "antd";
 import { FieldValues, SubmitHandler } from "react-hook-form";
 import OpenForm from "../../../components/form/OpenForm";
 import OpenInput from "../../../components/form/OpenInput";
@@ -16,12 +16,14 @@ const CreateStudent = () => {
       },
       gender: "male",
       dateOfBirth: "2000-01-01",
+      bloodGroup: "A+",
+
       email: "johndone2@gmail.com",
       contactNo: "1234567890",
       emergencyContactNo: "0987654321",
-      bloodGroup: "A+",
       presentAddress: "123 Street, City, State, Country",
       permanentAddress: "456 Avenue, City, State, Country",
+
       guardian: {
         fatherName: "Robert Smith",
         fatherOccupation: "Engineer",
@@ -58,15 +60,158 @@ const CreateStudent = () => {
           onSubmit={onSubmit}
           resolver={zodResolver(createStudentValidationSchema)}
         >
+          <Divider>Personal Information</Divider>
           <Row gutter={8}>
+            {/* Name */}
             <Col span={24} lg={{ span: 8 }} md={{ span: 12 }}>
-              <OpenInput label="First Name" name="firstName" type="text" />
+              <OpenInput label="First Name" name="name.firstName" type="text" />
             </Col>
             <Col span={24} lg={{ span: 8 }} md={{ span: 12 }}>
-              <OpenInput label="Middle Name" name="middleName" type="text" />
+              <OpenInput
+                label="Middle Name"
+                name="name.middleName"
+                type="text"
+              />
             </Col>
             <Col span={24} lg={{ span: 8 }} md={{ span: 12 }}>
-              <OpenInput label="Last Name" name="lastName" type="text" />
+              <OpenInput label="Last Name" name="name.lastName" type="text" />
+            </Col>
+            {/* Blood Group,DOB,Genger */}
+            <Col span={24} lg={{ span: 8 }} md={{ span: 12 }}>
+              <OpenInput label="Blood Group" name="bloodGroup" type="text" />
+            </Col>
+            <Col span={24} lg={{ span: 8 }} md={{ span: 12 }}>
+              <OpenInput label="Date Of Birth" name="dateOfBirth" type="text" />
+            </Col>
+            <Col span={24} lg={{ span: 8 }} md={{ span: 12 }}>
+              <OpenInput label="Gender" name="gender" type="text" />
+            </Col>
+
+            {/* Email,Contact No,emergencyContact no */}
+            <Divider>Contact Information</Divider>
+            <Col span={24} lg={{ span: 8 }} md={{ span: 12 }}>
+              <OpenInput label="Email" name="email" type="email" />
+            </Col>
+            <Col span={24} lg={{ span: 8 }} md={{ span: 12 }}>
+              <OpenInput label="Contact No" name="contactNo" type="text" />
+            </Col>
+            <Col span={24} lg={{ span: 8 }} md={{ span: 12 }}>
+              <OpenInput
+                label="Emergency Contact No"
+                name="emergencyContactNo"
+                type="text"
+              />
+            </Col>
+
+            {/* Present Address,Permanent Address */}
+            <Col span={24} lg={{ span: 8 }} md={{ span: 12 }}>
+              <OpenInput
+                label="Present Address"
+                name="presentAddress"
+                type="text"
+              />
+            </Col>
+            <Col span={24} lg={{ span: 8 }} md={{ span: 12 }}>
+              <OpenInput
+                label="Permanent Address"
+                name="permanentAddress"
+                type="text"
+              />
+            </Col>
+
+            {/* Guardian Information */}
+            <Divider>Guardian Information</Divider>
+
+            {/* Father Name,Father Occupation,Father Contact No */}
+            <Col span={24} lg={{ span: 8 }} md={{ span: 12 }}>
+              <OpenInput
+                label="Father Name"
+                name="guardian.fatherName"
+                type="text"
+              />
+            </Col>
+            <Col span={24} lg={{ span: 8 }} md={{ span: 12 }}>
+              <OpenInput
+                label="Father Occupation"
+                name="guardian.fatherOccupation"
+                type="text"
+              />
+            </Col>
+            <Col span={24} lg={{ span: 8 }} md={{ span: 12 }}>
+              <OpenInput
+                label="Father Contact No"
+                name="guardian.fatherContactNo"
+                type="text"
+              />
+            </Col>
+
+            {/* Mother Name,Mother Occupation,Mother Contact No */}
+            <Col span={24} lg={{ span: 8 }} md={{ span: 12 }}>
+              <OpenInput
+                label="Mother Name"
+                name="guardian.motherName"
+                type="text"
+              />
+            </Col>
+            <Col span={24} lg={{ span: 8 }} md={{ span: 12 }}>
+              <OpenInput
+                label="Mother Occupation"
+                name="guardian.motherOccupation"
+                type="text"
+              />
+            </Col>
+            <Col span={24} lg={{ span: 8 }} md={{ span: 12 }}>
+              <OpenInput
+                label="Mother Contact No"
+                name="guardian.motherContactNo"
+                type="text"
+              />
+            </Col>
+
+            {/* Local Guardian Information */}
+            <Divider>Local Guardian Information</Divider>
+            {/* Name,Occupation,Contact No */}
+            <Col span={24} lg={{ span: 8 }} md={{ span: 12 }}>
+              <OpenInput label="Name" name="localGuardian.name" type="text" />
+            </Col>
+            <Col span={24} lg={{ span: 8 }} md={{ span: 12 }}>
+              <OpenInput
+                label="Occupation"
+                name="localGuardian.occupation"
+                type="text"
+              />
+            </Col>
+            <Col span={24} lg={{ span: 8 }} md={{ span: 12 }}>
+              <OpenInput
+                label="Contact No"
+                name="localGuardian.contactNo"
+                type="text"
+              />
+            </Col>
+            {/* Address */}
+            <Col span={24} lg={{ span: 8 }} md={{ span: 12 }}>
+              <OpenInput
+                label="Address"
+                name="localGuardian.address"
+                type="text"
+              />
+            </Col>
+
+            {/* Admission Semester,Academic Department */}
+            <Divider>Academic Information</Divider>
+            <Col span={24} lg={{ span: 8 }} md={{ span: 12 }}>
+              <OpenInput
+                label="Admission Semester"
+                name="admissionSemester"
+                type="text"
+              />
+            </Col>
+            <Col span={24} lg={{ span: 8 }} md={{ span: 12 }}>
+              <OpenInput
+                label="Academic Department"
+                name="academicDepartment"
+                type="text"
+              />
             </Col>
           </Row>
           <Button type="primary" htmlType="submit">

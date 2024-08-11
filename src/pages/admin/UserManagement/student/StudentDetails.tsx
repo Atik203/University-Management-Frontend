f7f1a8f980bf953a2dd8ab1d4453e5b97a43f30c
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-08-12 01:00:31 +0600

    added student details page

--- src/pages/admin/UserManagement/student/StudentDetails.tsx
+++ src/pages/admin/UserManagement/student/StudentDetails.tsx
@@ -5,7 +5,9 @@ import { TStudent } from "../../../../types";
 const StudentDetails = () => {
   const { id } = useParams<{ id: string }>();
 
-  const { data, isFetching, isLoading } = useGetSingleStudentQuery(id);
+  const { data, isFetching, isLoading } = useGetSingleStudentQuery(
+    id as string
+  );
 
   if (isLoading || isFetching) {
     return <div>Loading...</div>;
@@ -13,8 +15,61 @@ const StudentDetails = () => {
   const student = data?.data as TStudent;
 
   return (
-    <div>
-      <h1>This is StudentDetails component {id}</h1>
+    <div
+      style={{
+        padding: "20px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "10px",
+        maxWidth: "500px",
+        margin: "auto",
+        fontSize: "1.2rem",
+      }}
+    >
+      <h1>Student Details</h1>
+      <p>
+        <strong>ID:</strong> {student.id}
+      </p>
+      <p>
+        <strong>Name:</strong> {student.fullName}
+      </p>
+      <p>
+        <strong>Email:</strong> {student.email}
+      </p>
+      <p>
+        <strong>Contact No:</strong> {student.contactNo}
+      </p>
+      <p>
+        <strong>Emergency Contact No:</strong> {student.emergencyContactNo}
+      </p>
+      <p>
+        <strong>Gender:</strong> {student.gender}
+      </p>
+      <p>
+        <strong>Date of Birth:</strong> {student.dateOfBirth}
+      </p>
+      <p>
+        <strong>Present Address:</strong> {student.presentAddress}
+      </p>
+      <p>
+        <strong>Permanent Address:</strong> {student.permanentAddress}
+      </p>
+      <p>
+        <strong>Guardian:</strong> {student.guardian.fatherName} /{" "}
+        {student.guardian.motherName}
+      </p>
+      <p>
+        <strong>Local Guardian:</strong> {student.localGuardian.name}
+      </p>
+      <p>
+        <strong>Admission Semester:</strong> {student.admissionSemester.name}
+      </p>
+      <p>
+        <strong>Academic Department:</strong> {student.academicDepartment.name}
+      </p>
+      <p>
+        <strong>Academic Faculty:</strong> {student.academicFaculty.name}
+      </p>
     </div>
   );
 };

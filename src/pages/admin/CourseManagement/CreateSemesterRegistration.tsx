bc79fa394c9516082b839f5015112a9db99a6325
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-08-18 00:26:56 +0600

    added routes for course management

---
+++ src/pages/admin/CourseManagement/CreateSemesterRegistration.tsx
@@ -0,0 +1,9 @@
+const CreateSemesterRegistration = () => {
+  return (
+    <div>
+      <h1>This is CreateSemesterRegistration component</h1>
+    </div>
+  );
+};
+
+export default CreateSemesterRegistration;
